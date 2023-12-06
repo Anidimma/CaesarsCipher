8c0b051dfadfe0f0997e46656e5b9f2e3f3f8204
Divinity <divineanigbogu@gmail.com>
2023-12-06 23:34:30 +0000

    adding to main

---
+++ src/main/java/com/mycompany/caesarscipher/LambdaConsumer.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.caesarscipher;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ *
+ * @author Divin
+ */
+
+public class LambdaConsumer{
+    public static void main(String[] args){
+        
+//        List<String> names = Arrays.asList("Alice", "Bob", "Charlie");
+//
+//        // Using a Consumer to print each element in the list
+//        Consumer<String> printName = name -> System.out.println(name);
+//
+//        // forEach method takes a Consumer
+//        names.forEach(printName);
+////
+//        // Using a lambda expression directly
+//        names.forEach(name -> System.out.println("Hello, " + name));
+        
+    }
+}
